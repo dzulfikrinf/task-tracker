f2be33b8073dff94bb5c8a0047f35037fcc9cf62
dzulfikrinf <xirpl2.dzulfikrinf@gmail.com>
2026-08-30 14:27:48 +0700

    add model relations for Project, Task, User

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
-    //
+    protected $fillable = ['name', 'description', 'user_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = ['project_id', 'title', 'description', 'status'];
+
+    public function project()
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'task_user');
+    }
 }
